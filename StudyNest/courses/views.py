4fd03aa2b78c129dcdb308c069b55175f9ccc899
ArbaazAfsar <168769189+ArbaazAfsar@users.noreply.github.com>
2024-08-10 14:38:32 +0530

    update never cash

--- StudyNest/courses/views.py
+++ StudyNest/courses/views.py
@@ -5,16 +5,19 @@ from .forms import CustomUserCreationForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
 def home(request):
     return render(request, 'courses/home.html',)
 
+@never_cache
 @login_required(login_url='/login/')
 def course_category(request):
     Category = CourseCategory.objects.all()
     return render(request, 'courses/CourseCategory.html',{'Category':Category} )
 
 
+@never_cache
 @login_required(login_url='/login/')
 def course_detail(request, foo):
     foo = foo.replace('-', ' ')
@@ -23,6 +26,7 @@ def course_detail(request, foo):
           
     return render(request, 'courses/courses.html', {'courses': courses, 'category_obj':category_obj})
 
+@never_cache
 @login_required(login_url='/login/')
 def lecture_detail(request, pk):
     course = get_object_or_404(Course, pk=pk)
@@ -30,7 +34,7 @@ def lecture_detail(request, pk):
     return render(request, 'courses/lecture_detail.html', {'lecture': lecture, 'course': course})
 
 
-
+@never_cache
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -42,7 +46,7 @@ def register(request):
         form = CustomUserCreationForm()
     return render(request, 'courses/register.html', {'form': form})
 
-
+@never_cache
 def user_login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -55,7 +59,7 @@ def user_login(request):
         form = AuthenticationForm()
     return render(request, 'courses/login.html', {'form': form})
 
-
+@never_cache
 def user_logout(request):
     logout(request)
     return redirect('home')  # Redirect to a home page or login page
